30d246647097cb28f8ba5c7764fc8b1f4de12163
archan0621 <joshua_0621@naver.com>
2023-02-28 02:40:17 +0000

    first commit

---
+++ NIC_Observer.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Get a list of all network interfaces starting with "eth"
+interfaces=$(ifconfig -a | grep -oP "^(eth[0-9]+)")
+
+# Loop through each interface and check its link status
+while true; do
+    for interface in $interfaces; do
+        # Use ethtool to get the link status of the interface
+        link_status=$(ethtool $interface | grep 'Link detected' | awk '{print $3}')
+
+        # Get the previous link status for the interface
+        prev_status_var="prev_link_status_$interface"
+        prev_status=$(eval "echo \$$prev_status_var")
+
+        # If the link status has changed, print a message and update the previous status
+        if [ "$prev_status" != "$link_status" ]; then
+            if [ "$link_status" = "yes" ]; then
+                echo "$interface is UP"
+            else
+                echo "$interface is DOWN"
+            fi
+            eval "$prev_status_var=$link_status"
+        fi
+    done
+
+    # Wait for one second before checking the link status again
+    sleep 1
+done
